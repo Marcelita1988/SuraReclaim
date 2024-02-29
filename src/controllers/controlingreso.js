104d33f76c60e72a12fee13673b19d1260b677de
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2024-02-29 16:38:11 -0500

    se crea controlador login

---
+++ src/controllers/controlingreso.js
@@ -0,0 +1,43 @@
+let usuarioBaseDatos="reclaim"
+let contrasenaBaseDatos="12345"
+
+
+let botonFormulario=document.getElementById("botonIngreso")
+let cajaFormularioNombre=document.getElementById("nombre")
+let cajaFormularioPassword=document.getElementById("password")
+
+//DETECTANDO EVENTOS CON JS
+botonFormulario.addEventListener("click",function(evento){
+
+    evento.preventDefault()
+    
+    //1.capturamos la informacion del formulario
+    let nombreUsuario=cajaFormularioNombre.value
+    let passwordUsario=cajaFormularioPassword.value
+
+    //validando los datos del uusuario
+    if(usuarioBaseDatos==nombreUsuario && contrasenaBaseDatos==passwordUsario){
+        Swal.fire({
+            title: "Bienvenido "+nombreUsuario,
+            text: "Tus credenciales son correctas",
+            icon: "success"
+        })
+
+        setTimeout(function(){
+            sessionStorage.setItem("nombre",nombreUsuario)
+            sessionStorage.setItem("password",passwordUsario)
+            window.location.href="./views/home.html"
+        },1000)
+        
+
+
+    }else{
+        Swal.fire({
+            icon: "error",
+            title: "Oops... "+nombreUsuario+"Tienes un problema",
+            text: "Verifica tus credenciales",
+            
+          });
+    }
+
+})
